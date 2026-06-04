d06c08b5a691194e87f3a49cba40a26dd88dfd22
Ramon Melendez <codebymelendez@gmail.com>
2026-06-04 09:31:38 +0200

    feat: add conditional Babel configuration to support both Metro and Next.js environments

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,14 @@
 module.exports = function (api) {
   api.cache(true)
+
+  const isMetro = api.caller((caller) => caller && (caller.name === 'metro' || caller.bundler === 'metro'))
+
+  if (!isMetro) {
+    return {
+      presets: ['next/babel'],
+    }
+  }
+
   return {
     presets: ['babel-preset-expo'],
     plugins: [
